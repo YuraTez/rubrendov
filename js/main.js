f1b1b70f34d920dd392ad584193c7af986ac3e70
Yura <yuratezher@mail.ru>
2023-11-01 14:10:28 +0300

    fix style

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 $(".burger").on("click" , function (){
     $(this).toggleClass("active");
     $(".nav").toggleClass("active");
+    $("body").toggleClass("no-scroll");
 })
 
 
 $(".nav-list__item").on("click" , function (){
     $(".burger").toggleClass("active");
     $(".nav").toggleClass("active");
+    $("body").removeClass("no-scroll");
 })
